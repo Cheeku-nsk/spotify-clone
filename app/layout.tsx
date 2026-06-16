418a42455fcc3c47f8153a1fe814b197439fd22a
Cheeku-nsk <kousikvivek71@gmail.com>
2026-06-16 22:09:48 +0530

    implemented sidebar

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+
+const font = Figtree({
+  variable: "--font-figtree-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Listen to music!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${font.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  );
+}
